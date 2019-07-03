eae3db16711731ba963b8d28d608d3fc0e322f62
Manuel Bieh <info@manuelbieh.de>
2019-07-03 13:54:55 +0200

    cleanup

--- webpack.config.js/index.js
+++ webpack.config.js/index.js
@@ -9,31 +9,3 @@ module.exports = (env = 'development') => {
 
     return require(`./config.${env}`);
 };
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//     entry: './src/index.js',
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.jsx?$/,
-//                 exclude: /node_modules/,
-//                 use: [{ loader: 'babel-loader' }],
-//             },
-//         ],
-//     },
-//     plugins: [new HtmlWebpackPlugin()],
-//     externals: {
-//         react: {
-//             commonjs2: 'react',
-//         },
-//     },
-//     mode: 'production',
-//     output: {
-//         path: path.resolve(__dirname, '..', 'lib'),
-//         libraryTarget: 'commonjs2',
-//         filename: 'index.js',
-//     },
-// };
